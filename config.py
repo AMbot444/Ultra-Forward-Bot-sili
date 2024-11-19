1901cccc6d5122d2df5f0bf7619a36b7cbd28008
AMbot444 <amitbhati51047@gmail.com>
2024-11-19 17:39:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,15 +6,15 @@ from os import environ
 # For Any Kind Of Error Ask Us In Support Group @Silicon_Botz 
 
 class Config:
-    API_ID = environ.get("API_ID", "")
-    API_HASH = environ.get("API_HASH", "")
+    API_ID = environ.get("API_ID", "25161407")
+    API_HASH = environ.get("API_HASH", "73bf36889a548e76e5132fb2b86d150e")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "Auto_Forward") 
     DATABASE_URI = environ.get("DATABASE", "")
     DATABASE_NAME = environ.get("DATABASE_NAME", "")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
-    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") 
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6658581796').split()]
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002223993999'))
+    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "-1002223993999") 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")
     PORT = environ.get('PORT', '8080')
     
